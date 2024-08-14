bb5106ffc47cb30842096ebbccce3aaf7cbc1269
aishwarya05-jadhav <aishwaryaj323@gmail.com>
2024-08-14 22:29:13 +0530

    new programs

--- MyJava/src/CollectionFramework/LearnArrayDeque.java
+++ MyJava/src/CollectionFramework/LearnArrayDeque.java
@@ -1,5 +1,26 @@
 package CollectionFramework;
 
-public class LearnArrayDeque {
+import java.util.ArrayDeque;
 
+//dequeue is a queue in which we can add and remove elements from both sides
+public class LearnArrayDeque {
+	public static void main(String[] args) {
+		ArrayDeque<Integer> adq = new ArrayDeque<>();
+		
+		adq.offer(1);
+		adq.offer(2);
+		adq.offer(3);
+		adq.offer(1);
+		adq.offer(5);
+		
+		adq.offerFirst(0);  //offer works as worked normally in queue i.e add element at last
+							//offerFirst adds element to head
+		adq.offerLast(100); //works same as offer
+		
+		System.out.println(adq);
+		
+		//similarly we can have peek(), peekFirst(), and peekLast()
+		//similarly we can have poll(), pollFirst(), and pollLast()
+	}
+	
 }
--- MyJava/src/CollectionFramework/LearnSet.java
+++ MyJava/src/CollectionFramework/LearnSet.java
@@ -1,5 +1,42 @@
 package CollectionFramework;
 
-public class LearnSet {
+import java.util.Set;
+import java.util.HashSet;
 
+public class LearnSet {
+	public static void main(String[] args) {
+		//In set duplicate elements are not allowed
+		
+		Set<Integer> set = new HashSet<>();
+		
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		set.add(5);
+		set.add(6);
+		
+		System.out.println(set);  //order does'nt matter in set
+		
+		set.remove(2);
+		System.out.println(set);
+		
+		System.out.println(set.contains(8));  //boolean
+		
+		System.out.println(set.isEmpty());  //boolean
+		
+		System.out.println(set.size()); //no of elements in set
+		
+		//set.clear();  //clear all elements in set
+		
+		/* LinkedHashSet is same as HashSet but additionaly it will also 
+		  show property of linkedlist and elements will be not added randomly 
+		  they will be added in given order*/
+		
+		/*  TreeSet is also similar to HashSet but in a sorted way
+		 * means it do it's implementation in form Binarysearch tree and also have properties of set
+		 * all operation in O(logn) time bcoz implemented using BST*/
+		
+		//in HashSet all operation in O(1) time
+	}
 }
